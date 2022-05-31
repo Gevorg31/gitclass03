01b50a7839b81ba066dfabd40d0589bea46d089e
Gevorg31 <gevorgnyus90@gmail.com>
2022-05-31 19:54:30 -0400

    2nd commit

--- src/github/gitAndGitHub.java
+++ src/github/gitAndGitHub.java
@@ -2,4 +2,10 @@ package github;
 
 public class gitAndGitHub {
 
+	public static void main(String[] args) {
+
+		System.out.println("Today we learn github");
+		System.out.println("Today we learn github");
+	}
+
 }
